c1c7963eb8cdd7d3510d2cdd6ac25c4e0e8d3646
Adam Cardoso <adamjuniorcardoso@gmail.com>
2022-01-09 14:05:47 -0300

    Estado e Cidade

---
+++ src/main/java/com/adamcardoso/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,10 @@
+package com.adamcardoso.cursomc.repositories;
+
+import com.adamcardoso.cursomc.domain.Cidade;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+}
