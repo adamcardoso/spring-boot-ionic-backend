5b6135a62acba143c0f57e245369dcfaac084cd7
Adam Cardoso <adamjuniorcardoso@gmail.com>
2022-01-12 15:20:02 -0300

    atualizando categoria com PUT, outros pequenos ajustes

--- src/main/java/com/adamcardoso/cursomc/services/CategoriaService.java
+++ src/main/java/com/adamcardoso/cursomc/services/CategoriaService.java
@@ -24,4 +24,10 @@ public class CategoriaService {
         obj.setId(null);
         return repo.save(obj);
     }
+
+    public Categoria update(Categoria obj) {
+        find(obj.getId()); //verificação se o id existe, se não existe, chama a excessão
+
+        return repo.save(obj);
+    }
 }
