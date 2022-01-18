da5a4b2aa81299d4a58da3baeeb3c1eab209e689
Adam Cardoso <adamjuniorcardoso@gmail.com>
2022-01-18 14:52:42 -0300

    Validação sintática com Bean Validation:

--- src/main/java/com/adamcardoso/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/adamcardoso/cursomc/dto/CategoriaDTO.java
@@ -1,9 +1,11 @@
 package com.adamcardoso.cursomc.dto;
 
 import com.adamcardoso.cursomc.domain.Categoria;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.validation.constraints.NotEmpty;
 import javax.persistence.Id;
 import java.io.Serializable;
 
@@ -14,6 +16,8 @@ public class CategoriaDTO implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) //geração automática dos IDs
     private Integer id;
+    @NotEmpty(message = "Preenchimento obrigatório")
+    @Length(min = 5, max = 80, message = "O tamanho deve ser entre 5 e 80 caracteres")
     private String nome;
 
     public CategoriaDTO() {
