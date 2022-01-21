f44dce211edcfc9e8e268e002ea94010d24721f9
Adam Cardoso <adamjuniorcardoso@gmail.com>
2022-01-21 14:55:52 -0300

    Validacao customizada de CPF e CNPJ na insercao de Cliente

---
+++ src/main/java/com/adamcardoso/cursomc/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,49 @@
+package com.adamcardoso.cursomc.services.validation;
+
+import com.adamcardoso.cursomc.domain.enums.TipoCliente;
+import com.adamcardoso.cursomc.dto.ClienteNewDTO;
+import com.adamcardoso.cursomc.repositories.ClienteRepository;
+import com.adamcardoso.cursomc.resources.exceptions.FieldMessage;
+import com.adamcardoso.cursomc.services.validation.utils.BR;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+    @Autowired
+    private ClienteRepository repo;
+
+    @Override
+    public void initialize(ClienteInsert ann) {
+    }
+
+    @Override
+    public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+
+        List<FieldMessage> list = new ArrayList<>();
+
+        if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfouCnpj())) {
+            list.add(new FieldMessage("cpfouCnpj", "CPF inválido"));
+        }
+
+        if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfouCnpj())) {
+            list.add(new FieldMessage("cpfouCnpj", "CNPJ inválido"));
+        }
+
+        /*Cliente aux = repo.findByEmail(objDto.getEmail());
+        if (aux != null) {
+            list.add(new FieldMessage("email", "Email já existente"));
+        }*/
+
+        for (FieldMessage e : list) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+                    .addConstraintViolation();
+        }
+        return list.isEmpty();
+    }
+}
