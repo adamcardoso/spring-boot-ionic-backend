0d61f57ca3425cdb56228d2d6840236a2b4bd77f
Adam Cardoso <adamjuniorcardoso@gmail.com>
2022-01-26 16:06:50 -0300

    MockEmailService com Logger. Padrões Strategy e Template Method

--- src/main/java/com/adamcardoso/cursomc/config/TestConfig.java
+++ src/main/java/com/adamcardoso/cursomc/config/TestConfig.java
@@ -1,6 +1,8 @@
 package com.adamcardoso.cursomc.config;
 
 import com.adamcardoso.cursomc.services.DBService;
+import com.adamcardoso.cursomc.services.EmailService;
+import com.adamcardoso.cursomc.services.MockEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,4 +22,10 @@ public class TestConfig {
         dbService.instantiateTestDatabase();
         return true;
     }
+
+    @Bean
+    public EmailService emailService(){
+        return new MockEmailService();
+    }
+
 }
---
+++ src/main/java/com/adamcardoso/cursomc/services/EmailService.java
@@ -0,0 +1,11 @@
+package com.adamcardoso.cursomc.services;
+
+import com.adamcardoso.cursomc.domain.Pedido;
+import org.springframework.mail.SimpleMailMessage;
+
+public interface EmailService {
+
+    void sendOrderConfirmationEmail(Pedido obj);
+
+    void sendEmail(SimpleMailMessage msg);
+}
