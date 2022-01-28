805d1364145b29a5282821ad16e1e4810699feae
Adam Cardoso <adamjuniorcardoso@gmail.com>
2022-01-28 16:01:48 -0300

    Implementando autenticação e geração do token JWT

---
+++ src/main/java/com/adamcardoso/cursomc/services/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.adamcardoso.cursomc.services;
+
+import com.adamcardoso.cursomc.domain.Cliente;
+import com.adamcardoso.cursomc.repositories.ClienteRepository;
+import com.adamcardoso.cursomc.security.UserSS;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Cliente cli = clienteRepository.findByEmail(email);
+
+        if (Objects.isNull(cli)){
+            throw new UsernameNotFoundException(email);
+        }
+
+        return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfis());
+    }
+}
