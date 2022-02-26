8e9a7b1bd1d21f102f8d629de10664caaaf2edf2
Adam Cardoso <adamjuniorcardoso@gmail.com>
2022-02-26 17:27:10 -0300

    Enviando imagem via endpoint

--- src/main/java/com/adamcardoso/cursomc/CursomcApplication.java
+++ src/main/java/com/adamcardoso/cursomc/CursomcApplication.java
@@ -9,15 +9,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
-    @Autowired
-    private S3Service s3Service;
-
     public static void main(String[] args) {
         SpringApplication.run(CursomcApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-        s3Service.uploadFile("C:\\tempAWS\\horizon.jpg");
+
     }
 }
